17f88da2af6310ce1cdab115e2f3a10e2555ad8e
Rodrigo Viturino <viturino.souza1@gmail.com>
2018-06-14 21:29:49 -0300

    Adicionando Controllers,Viewer,Model
    Iniciando angular com o MVC

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,7 @@
+angular.module('alurapic').controller('FotosController', function($scope) {
+
+   $scope.foto = {
+        titulo:'Angular',
+        url:'http://www.php-training.in/images/services/angularjs-training-mumbai.png'
+    };
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,3 @@
-angular.module('projeto', 
-    ['ngResource','ngRoute', 'ngAnimate', 'ngCookies', 'Picture', 'Panel', 'FotoService'])
-    .config(function($routeProvider, $locationProvider) {
+// Criando o modulo e e podemos colocar outros modulos COMO DEPENDENCIA no ARRAY
+angular.module('alurapic',[]); 
 
-        $locationProvider.html5Mode(true);
-
-        $routeProvider.when('/fotos', {
-            templateUrl: 'partials/principal.html',
-            controller: 'PrincipalController'
-        });
-
-        $routeProvider.when('/fotos/new', {
-            templateUrl: 'partials/foto.html',
-            controller: 'FotoController'
-        });
-
-        $routeProvider.when('/fotos/edit/:fotoId', {
-            templateUrl: 'partials/foto.html',
-            controller: 'FotoController'
-        });
-
-        $routeProvider.otherwise({redirectTo: '/fotos'});
-
-    });
